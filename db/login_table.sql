aa7a4b24b066e5162800670e14442142c96b9cfb
Fernando Vinícius Gianini Silva <gianini@angularlabs.com.br>
2020-06-17 02:37:57 -0300

    Criada função de login

--- db/login_table.sql
+++ db/login_table.sql
@@ -4,6 +4,8 @@ DROP TABLE IF EXISTS Usuario CASCADE;
 DROP SEQUENCE IF EXISTS Log_Acesso_seq;
 DROP SEQUENCE IF EXISTS Usuario_seq;
 
+DROP FUNCTION login_banco(tentar_login varchar, tentar_senha varchar);
+
 -- Add extension for encryption
 CREATE EXTENSION pgcrypto;
 
@@ -18,6 +20,7 @@ CREATE TABLE Usuario (
 	senha VARCHAR(255) NOT NULL,
 
 	CONSTRAINT usuario_pk PRIMARY KEY (id_usuario)
+
 );
 
 CREATE TABLE Log_Acesso (
@@ -30,12 +33,41 @@ CREATE TABLE Log_Acesso (
 );
 
 -- New sequences
-CREATE SEQUENCE Usuario_seq START 1;
-CREATE SEQUENCE Log_Acesso_seq START 1;
+CREATE SEQUENCE usuario_seq START 1;
+CREATE SEQUENCE log_acesso_seq START 1;
 
 -- New data
 INSERT INTO Usuario (id_usuario, papel, login, senha)
-VALUES (1, 'ADMIN', 'adminCovid', crypt('superAdmin', gen_salt('md5')));
+VALUES (nextval('usuario_seq'), 'ADMIN', 'adminCovid', crypt('superAdmin', gen_salt('md5')));
 
 INSERT INTO Log_Acesso (id_log, id_usuario, data_acesso)
-VALUES (1, 1, CURRENT_TIMESTAMP);
+VALUES (nextval('log_acesso_seq'), 1, CURRENT_TIMESTAMP);
+
+-- Access database through login
+CREATE OR REPLACE FUNCTION login_banco(tentar_login varchar, tentar_senha varchar) RETURNS BOOLEAN AS '
+	DECLARE
+		has_login INTEGER;
+		has_logged BOOLEAN;
+	BEGIN
+		SELECT 	U.id_usuario INTO has_login
+		FROM 	Usuario U
+		WHERE 	U.login = tentar_login;
+
+		IF has_login IS NOT NULL THEN
+			SELECT U.senha = crypt(''superAdmin'', U.senha) INTO has_logged FROM Usuario U WHERE U.id_usuario = has_login;
+
+			IF has_logged = TRUE THEN
+
+				INSERT INTO Log_Acesso(id_log, id_usuario, data_acesso)
+				VALUES (nextval(''log_acesso_seq''), has_login, CURRENT_TIMESTAMP);
+				RETURN TRUE;
+
+			ELSE
+				RETURN FALSE;
+			END IF;
+		ELSE
+			RETURN FALSE;
+		END IF;
+	END;
+'
+LANGUAGE plpgsql;
