a431afcbfde84971aa367e68c64a720963e4b69c
Fernando Vinícius Gianini Silva <gianini@angularlabs.com.br>
2020-06-17 03:07:35 -0300

    Menu simples

--- DatabaseController.py
+++ DatabaseController.py
@@ -8,9 +8,10 @@ class PGDatabase:
         """
         """
         self.connection = None
+        self.user = ''
 
         try:
-            connection = psycopg2.connect(database='COVID-19',
+            self.connection = psycopg2.connect(database='COVID-19',
                              user = 'postgres',
                              password = 'postgres',
                              host = '127.0.0.1',
@@ -19,5 +20,31 @@ class PGDatabase:
             print('Conectado com o banco de dados!\n\n')
         except Exception as e:
             print('Não foi possível se conectar ao banco de dados!')
+            print('Fechando programa...')
             raise e
+
+    """ GETTERS E SETTERS
+    """
+    def getConnection(self):
+        return self.connection
+
+    def setUser(self, user):
+        self.user = user
+
+    def getUser(self):
+        return self.user
+
+    def loginDatabase(self, login, password):
+        cursor = self.getConnection().cursor()
+
+        cursor.callproc('login_banco', (login, password))
+
+        result = cursor.fetchone()
+
+        if result[0] == True:
+            print('Usuário {} logado!'.format(login))
+            self.setUser(login)
+            return True
+
+        return False
 
--- main.py
+++ main.py
@@ -2,3 +2,41 @@ from DatabaseController import *
 
 db = PGDatabase()
 
+print('****COVID-19 DASHBOARD - OMS****\n')
+
+print('Primeiro, realize o login (entre com login=0 para fechar o aplicativo):')
+
+loginSuccess = False
+login = ''
+
+while loginSuccess == False:
+	login = input('\tLogin: ')
+
+	if login == '0':
+		break
+
+	senha = input('\tSenha: ')
+
+	loginSuccess = db.loginDatabase(login, senha)
+
+if login != '0':
+	op = '-1'
+	print('\n---------------\n')
+	while op != '0':
+		print('\n****TELA PRINCIPAL****\n')
+		print('\t1 - Dashboard')
+		print('\t2 - Relatórios')
+		print('\t3 - Simulações')
+		print('\t0 - Sair')
+		op = input('\n\tOpção: ')
+
+		if op == '1':
+			print('\n***DASHBOARD***\n')
+		elif op == '2':
+			print('\n***RELATÓRIOS***\n')
+		elif op == '3':
+			print('\n***SIMULAÇÕES***\n')
+		elif op == '0':
+			print('Saindo do programa...')
+		else:
+			print('Opção inválida!')
